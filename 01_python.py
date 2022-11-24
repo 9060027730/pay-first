b8192350df7c2d86a02519fb9361cd324927331d
9060027730 <118840603+9060027730@users.noreply.github.com>
2022-11-24 15:17:23 +0530

    Add files via upload

---
+++ 01_python.py
@@ -0,0 +1,122 @@
+#print("Enter your number")
+#n1 = input()
+#print("Enter your second number")
+#n2 = input()
+
+#print("sum =",int(n1) + int(n2))
+
+
+str = "hurry is good boy"
+#print(str.isupper())
+#print(str.islower())
+#print(str.isalnum())
+#print(str.isalnum())
+#print(str.endswith("boy"))
+#print(str.count("o"))
+#print(str.find("is"))
+#print(str.replace("is", "are"))
+#print(type(str))
+#print(str)
+#print(len(str))
+#print(str[::-1])
+
+
+
+
+
+
+
+grocery = ["harpic", "vim bar", "deodrant", "Bhind", "looypop"]
+#print(grocery[3])
+numbers = [2, 4, 5, 6, 7, 11]
+#numbers.reverse()
+#print(numbers)
+#print(numbers[1:4])
+#print(numbers[::2])
+#print(len(numbers))
+#
+# print(min(numbers))
+#print(max(numbers))
+
+#numbers.append(7)
+#print(numbers)
+#numbers.insert(1, 44)
+#numbers.pop
+#print(numbers)
+#numbers[2] = 55
+#print(numbers)
+#tp = (1,3,5)
+#aq =1
+#print(tp)
+#a = 1
+#b = 5
+#a, b=b, a
+#temp = a
+#a = b
+#b = temp
+#print(a, b)
+
+
+# dictionary define
+#d1 = {}
+#print(type(d1))
+d2 = {"Harry":"burger",
+       "Rohan":"fish"}
+#print(d2["Harry"])
+
+#d2[420] = "kababs"
+#d2["Raju"]="maawa"
+#del d2["Raju"]  # del (delete karta ka koi function ko)
+#print(d2)
+#d3 = d2
+#del d3["Hurry"]
+#print(d2)
+##d2.copy()
+#print(d2)
+#d2.items()
+#print(d2)
+#d2.keys()
+#print(d2)
+#d2.values()
+
+
+s = set()
+#print(type(s))
+#s_from_list = ([1, 2, 3, 4, 5])
+#print(s_from_list)
+#print(type(s_from_list))
+s.add(1)
+s.add(2)
+#s1 = s.intersection({1, 2, 3})
+#print(s)
+#print(s, s1)
+#print(len(s))
+#print(len(s1))
+#print(type(s))
+#print(min(s))
+#print(type(s1))
+#print(min(s1))
+#print(max(s))
+
+#print(max(s1))
+#print(s.isdisjoint(s1))
+
+var1 = 4
+var2 = 55
+var3 = int(input())
+
+if var3>var2:
+      print("Greater")
+
+else:
+      print("Lesser")
+      
+list1 = [3,5,7,]
+
+print("enter your age?")
+age = int(input())
+
+
+
+
+      
